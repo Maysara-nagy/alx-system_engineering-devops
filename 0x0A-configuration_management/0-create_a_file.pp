131da821c9d75741cc9d8bf8a681227d4d6f7170
Maysara-nagy <maysara.nagy@gmail.com>
2023-06-10 16:40:34 -0700

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+file { '/tmp/school' :
+  ensure  => 'file',
+  path    => '/tmp/school',
+  mode    => '0744',
+  group   => 'www-data',
+  owner   => 'www-data'
+  content => 'I love Puppet'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+this is my first cm projet using puppet lang
