896b996e3216c806c74315023e03cd0b11b6536b
Maysara-nagy <maysara.nagy@gmail.com>
2023-06-11 00:06:23 -0700

    try

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 #create file school
-file { '/tmp/school' :
-  ensure  => 'file',
+file { '/tmp/school':
+  ensure  => file,
   path    => '/tmp/school',
   mode    => '0744',
   group   => 'www-data',
