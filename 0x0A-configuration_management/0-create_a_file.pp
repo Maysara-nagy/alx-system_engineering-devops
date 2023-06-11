7054a2776910b12fb396f43598eadbb2ac1cc04a
Maysara-nagy <maysara.nagy@gmail.com>
2023-06-11 00:07:28 -0700

    try

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-#create file school
+# Creates a file
 file { '/tmp/school':
   ensure  => file,
   path    => '/tmp/school',
   mode    => '0744',
+  owner   => 'www-data',
   group   => 'www-data',
-  owner   => 'www-data'
   content => 'I love Puppet'
 }
